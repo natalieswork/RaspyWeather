f6bc5f2e5691c3b121bce13ffd65ddc8b1a8c413
natalieswork <natalieleecodes@gmail.com>
2023-11-06 14:37:23 -0500

    working app bones

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Raspy Weather is a Flask web application deployed on a Raspberry Pi that allows 
 
 - From a terminal opened at this project's directory, create a virtual environment: `python -m venv .venv`
 - Activate the virtual environment: `source .venv/bin/activate`
-- Install requirements: `pip install r requirements.txt`
-- Run the code in `.py`
+- Install requirements: `pip install -r requirements.txt`
+- Run the code in `app.py`
+- View app on local host link. `http://127.0.0.1:5000`
--- weather.py
+++ weather.py
@@ -1,6 +1,16 @@
 import requests
 from dotenv import load_dotenv
 import os
+from dataclasses import dataclass
+
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: float
+
 
 load_dotenv()
 api_key = os.getenv('API_KEY')
@@ -8,8 +18,38 @@ api_key = os.getenv('API_KEY')
 
 def get_lat_lon(city_name, state_code, country_code, API_key):
     resp = requests.get(
-        f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
-    print(resp)
+        f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    data = resp[0]
+    lat, lon = data.get('lat'), data.get('lon')
+    return lat, lon
 
 
-get_lat_lon('Toronto', 'ON', 'Canada', api_key)
+def get_current_weather(lat, lon, API_key):
+    resp = requests.get(
+        f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API_key}&units=imperial').json()
+    current_weather = resp.get('current', {})
+
+    if 'weather' in current_weather and len(current_weather['weather']) > 0:
+        weather_info = current_weather['weather'][0]
+        main = weather_info.get('main')
+        description = weather_info.get('description')
+        icon = weather_info.get('icon')
+    else:
+        main = description = icon = "N/A"
+
+    temperature = current_weather.get('temp', "N/A")
+
+    data = WeatherData(main=main, description=description,
+                       icon=icon, temperature=temperature)
+    return data
+
+
+def main(city_name, state_name, country_name):
+    lat, lon = get_lat_lon(city_name, state_name, country_name, api_key)
+    weather_data = get_current_weather(lat, lon, api_key)
+    return weather_data
+
+
+if __name__ == "__main__":
+    lat, lon = get_lat_lon('Toronto', 'ON', 'Canada', api_key)
+    print(get_current_weather(lat, lon, api_key))
